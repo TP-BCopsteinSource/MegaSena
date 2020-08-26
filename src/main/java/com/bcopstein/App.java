16158561ce5faa580a8af4d98411e729701bb6b1
Bernardo Copstein <bernardo.copstein@pucrs.br>
2020-08-26 17:03:09 -0300

    Novo POM para Java 14

--- src/main/java/com/bcopstein/App.java
+++ src/main/java/com/bcopstein/App.java
@@ -8,9 +8,11 @@ public class App
 {
     public static void main( String[] args )
     {
+        System.out.println("\nResultados da Mega Sena:");
         String[] resultado = ResultadoMegasena.obtemUltimoResultado();
         for (String dezena: resultado) {
               System.out.print(dezena + " ");
         }
+        System.out.println("\n");
     }
 }
